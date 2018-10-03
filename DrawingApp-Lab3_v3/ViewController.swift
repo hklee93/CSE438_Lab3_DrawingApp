af6ca8effa8c9e8a5bee3e5aa989aef999b8f17b
Hakkyung <hakkyung@wustl.edu>
2018-10-03 17:46:15 -0500

    Initial Commit

---
+++ DrawingApp-Lab3_v3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DrawingApp-Lab3_v3
+//
+//  Created by Hakkyung on 2018. 10. 3..
+//  Copyright © 2018년 Hakkyung Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
